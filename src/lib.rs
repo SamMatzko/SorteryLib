f0c2513ab8fa15fbd473719eef07d03fa5c871bf
Samuel Matzko <matzko+sam@gmail.com>
2021-12-31 14:02:52 -0500

    📝 Finished the documentation for src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,37 @@
 //! SorteryLib is a fast file-sorting library based on the Sortery command-line file sorter.
+//! 
+//! Basic usage example:
+//! 
+//! ```rust
+//! use sorterylib::prelude::*; // Import all the stuff needed for basic operation
+//! 
+//! fn main() {
+//!     // The fields for initializing the Sorter struct
+//!     let source = File::new("/path/to/source/dir/");
+//!     let target = File::new("/path/to/target/dir/");
+//!     let date_format = String::from("%Y");
+//!     let date_type = String::from("m");
+//!     let preserve_name = true;
+//!     let exclude_type = vec![String::from("txt")];
+//!     let only_type = Vec::new();
+//! 
+//!     // Create the Sorter instance
+//!     let sorter = Sorter {
+//!         source: source.copy(), // The directory from which to get all the files to sort
+//!         target: target.copy(), // The directory to sort all the files into
+//!         date_format: date_format, // The date format to rename the files using.
+//!         date_type: date_type, // The date type to sort the files by
+//!         preserve_name: preserve_name, // Whether to include the old file name in the new name
+//!         exclude_type: exclude_type, // File type(s) to exclude
+//!         only_type: only_type // File type(s) to exclusively sort. Overrides `exclude_type`
+//!     };
+//! 
+//!     // Run the sorting algorithm
+//!     sorter.sort(false);
+//! }
+//! ```
+//! 
+//! You can find more detailed descriptions of the fields on the [`Sorter`] page.
 
 #![cfg(target_os = "linux")]
 pub mod structs;
@@ -57,16 +90,35 @@ mod tests {
     }
 }
 
-/// The sorter struct that sorts the files, and interfaces with it's caller for
-/// progress bar purposes, if desired.
+/// The sorter struct that sorts the files. There are two ways to create an instance
+/// of [`Sorter`]: passing the individual fields, and using [`Sorter::from_json`].
 #[derive(Debug, PartialEq)]
 pub struct Sorter {
+    /// An instance of [`File`] specifying the directory from which to get
+    /// all the files to sort
     pub source: File,
+    /// An instance of [`File`] specifying the directory to sort all the
+    /// files into.
     pub target: File,
+    /// A [`String`] representing the date format. Uses the standard `strftime` format.
+    /// See [`chrono::format::strftime`] for formatting information.
     pub date_format: String,
+    /// A [`String`] representing the date type to sort by. Must be one of `String::from("a")`
+    /// (accessed) `String::from("c")` (created), or `String::from("m")` (modified).
+    /// Note that sorting by creation date is not available on all filesystems.
     pub date_type: String,
+    /// If [`true`], then the sorter adds the old file name onto the end of the new
+    /// one. For example, `test.txt` would be renamed to something like `2021-04-22 test.txt`.
+    /// If [`false`], then ignores the old file name (would rename `test.txt` to `2021-04-22.txt`).
     pub preserve_name: bool,
+    /// A [`Vec<String>`] containing all the file extensions to be ignored during
+    /// sorting. For example, if `vec![String::from("jpg")]` is passed, than all files
+    /// ending in `.jpg` won't be sorted.
     pub exclude_type: Vec<String>,
+    /// A [`Vec<String>`] containing all the file extensions to be exclusively sorted.
+    /// For example, if `vec![String::from("png")] is passed, than *only* files ending
+    /// in `.png` will be sorted. All other files will be ignored. This option overrides
+    /// `exclude_type`.
     pub only_type: Vec<String>
 }
 impl Sorter {
@@ -74,7 +126,31 @@ impl Sorter {
     // Class functions
 
     /// Return a new [`Sorter`] instance, created using the configuration data in
-    /// a JSON string
+    /// a JSON string. The source and target directories must be passed to [`Sorter::from_json`]
+    /// as well as the json string. For example:
+    /// 
+    /// ```rust
+    /// use sorterylib::prelude::*;
+    /// use std::{fs, path::Path};
+    /// 
+    /// fn main() {
+    /// 
+    ///     // The path to the JSON file
+    ///     let json_path = Path::new("test.json");
+    /// 
+    ///     // Load the JSON string from the JSON file
+    ///     let json_string = fs::read_to_string(json_path).expect("Failed to read file");
+    /// 
+    ///     // Create a Sorter instance based on the json file
+    ///     let sorter = Sorter::from_json(
+    ///         json_string,
+    ///         File::from("/path/to/source/dir/"), // The source directory
+    ///         File::from("/path/to/target/dir/") // The target directory
+    ///     );
+    /// }
+    /// ```
+    /// 
+    /// In future versions, a JSON file will be able to be passed instead of a JSON [`String`].
     pub fn from_json(json_string: String, source: File, target: File) -> Sorter {
 
         // Get the data from the JSON string
@@ -140,6 +216,7 @@ impl Sorter {
         secs
     }
 
+    /// Get the new directory stacks for all the files, according to the sorting algorithm.
     fn get_new_date_path(
         &self,
         target: &File,
@@ -201,6 +278,7 @@ impl Sorter {
         }
     }
 
+    /// Get the full sorting results for all the files according to the sorting algorithm.
     fn get_sorting_results(
         &self,
         source: &File,
@@ -286,8 +364,31 @@ impl Sorter {
         to_return
     }
 
-    /// The method that runs the sorting algorithm, and sends information through
-    /// to the caller if specified.
+    /// The method that runs the sorting algorithm. Returns the sorting results as
+    /// a tuple of ([`usize`], [`Vec<String>`], [`Vec<String>`]), where `results.0`
+    /// is the number of items sorted, `results.1` contains all the old file names,
+    /// and `results.2` contains all the new file names. The two vectors correspond
+    /// index-wise, so `results.1[0]` is renamed to `results.2[0]`, etc.
+    /// 
+    /// If `dry_run` is [`true`], return the results as usual, but without acutally
+    /// sorting the files. Can be used to verify that the sorting algorithm is working
+    /// as intended. For example:
+    /// 
+    /// ```rust
+    /// use sorterylib::prelude::*;
+    /// 
+    /// fn main() {
+    /// 
+    ///     // The sorter instance
+    ///     let sorter = Sorter { ... };
+    /// 
+    ///     // Dry run, without actually sorting the files
+    ///     sorter.sort(true);
+    /// 
+    ///     // Acutally sort the files
+    ///     sorter.sort(false);
+    /// }
+    /// ```
     pub fn sort(&self, dry_run: bool) -> (usize, Vec<File>, Vec<File>) {
 
         // Convert the exclude_type and only_type values to the tuples that
