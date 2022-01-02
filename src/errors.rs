4f6838c910f1cd14751929f9b1acb04be5cd9c35
Samuel Matzko <matzko+sam@gmail.com>
2022-01-02 18:13:48 -0500

    Added a colorful panic message for when the user tries to sort with nonexistent source or target.

---
+++ src/errors.rs
@@ -0,0 +1,15 @@
+use colored::Colorize;
+use std::fmt;
+
+/// The error message shown when the user tries to sort with source and/or target
+/// directories that don't exist
+#[derive(Clone, Debug)]
+pub struct PathDoesNotExistError {
+    pub path: String,
+}
+impl fmt::Display for PathDoesNotExistError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{} path \"{}\" does not exist.", format!("Error:").red(), format!("{}", self.path).bold())
+    }
+}
+impl std::error::Error for PathDoesNotExistError { }
